d979bafcc7048cbdfc6147255d147b68b81bf1d3
X.Young <tuantuan0515@gmail.com>
2025-09-07 20:21:50 +0100

    use seq2seq_im_mask_obj in OEM 2

--- OEM/models/model/seq2seq_im_mask_obj.py
+++ OEM/models/model/seq2seq_im_mask_obj.py
@@ -7,7 +7,7 @@ import collections
 from torch import nn
 from torch.nn import functional as F
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
-from model.seq2seq import Module as Base
+from OEM.models.model.seq2seq import Module as Base
 from models.utils.metric import compute_f1, compute_exact
 from gen.utils.image_util import decompress_mask
 
@@ -18,7 +18,7 @@ import constants
 classes = [0] + constants.OBJECTS + ['AppleSliced', 'ShowerCurtain', 'TomatoSliced', 'LettuceSliced', 'Lamp',
                                      'ShowerHead', 'EggCracked', 'BreadSliced', 'PotatoSliced', 'Faucet']
 
-from nn.resnet import Resnet
+from OEM.models.nn.resnet import Resnet
 
 
 class SelfAttn(nn.Module):
