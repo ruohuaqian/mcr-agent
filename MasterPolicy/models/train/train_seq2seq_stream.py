cf84690fc8c010a4b7aed9038cb73a7fe88cff4d
X.Young <tuantuan0515@gmail.com>
2025-09-12 07:31:58 +0100

    MasterPolicy train support streaming.

--- MasterPolicy/models/train/train_seq2seq_stream.py
+++ MasterPolicy/models/train/train_seq2seq_stream.py
@@ -8,6 +8,7 @@ from importlib import import_module
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from MasterPolicy.models.utils.helper_utils import optimizer_to
 from huggingface_hub import hf_hub_download
+from MasterPolicy.models.model.navigation_streaming_trainer import NavigationStreamingTrainer
 
 
 torch.backends.cudnn.enabled = False
