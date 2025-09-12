27a52a2b1225d3aa15f5f552e43480ebb9360c58
X.Young <tuantuan0515@gmail.com>
2025-09-12 02:18:34 +0100

    Interactions train support streaming.

--- Interactions/models/train/train_seq2seq_stream.py
+++ Interactions/models/train/train_seq2seq_stream.py
@@ -115,4 +115,4 @@ if __name__ == '__main__':
             optimizer_to(optimizer, torch.device('cuda'))
 
     # start train loop
-    model.run_train_stream(splits, optimizer=optimizer)
+    model.run_train_stream(splits, optimizer=optimizer, args=args, vocab=vocab)
