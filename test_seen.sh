48f0dc48b755c6fc5005707241b71fc4465a7ac3
X.Young <tuantuan0515@gmail.com>
2025-09-12 11:02:44 +0100

    test shell

--- test_seen.sh
+++ test_seen.sh
@@ -1,18 +1,18 @@
 python models/eval/eval_seq2seq.py   \
-		--nav_model_path exp/MasterPolicy/net_epoch_best.pth  \
-		--pickup_model_path exp/PickupObject/net_epoch_best.pth  \
-		--put_model_path exp/PutObject/net_epoch_best.pth  \
-		--heat_model_path exp/HeatObject/net_epoch_best.pth  \
-		--cool_model_path exp/CoolObject/net_epoch_best.pth  \
-		--clean_model_path exp/CleanObject/net_epoch_best.pth  \
-		--toggle_model_path exp/ToggleObject/net_epoch_best.pth  \
-		--slice_model_path exp/SliceObject/net_epoch_best.pth  \
-		--object_model_path exp/OEM/net_epoch_best.pth  \
-		--subgoal_model_path exp/PCC/net_epoch_best.pth  \
+		--nav_model_path /content/drive/MyDrive/mcr-agent/exp/MasterPolicy/latest.pth  \
+		--pickup_model_path /content/drive/MyDrive/mcr-agent/exp/Interactions/PickupObject/latest.pth  \
+		--put_model_path /content/drive/MyDrive/mcr-agent/exp/Interactions/PutObject/latest.pth  \
+		--heat_model_path /content/drive/MyDrive/mcr-agent/exp/Interactions/HeatObject/latest.pth  \
+		--cool_model_path /content/drive/MyDrive/mcr-agent/exp/Interactions/CoolObject/latest.pth  \
+		--clean_model_path /content/drive/MyDrive/mcr-agent/exp/Interactions/CleanObject/latest.pth  \
+		--toggle_model_path /content/drive/MyDrive/mcr-agent/exp/Interactions/ToggleObject/latest.pth  \
+		--slice_model_path /content/drive/MyDrive/mcr-agent/exp/Interactions/SliceObject/latest.pth  \
+		--object_model_path /content/drive/MyDrive/mcr-agent/exp/OEM/latest.pth  \
+		--subgoal_model_path /content/drive/MyDrive/mcr-agent/exp/PCC/latest.pth  \
 		--eval_split	valid_seen                          \
-		--data		data/json_feat_2.1.0                \
+		--huggingface_id		byeonghwikim/abp dataset       \
 		--gpu                                               \
-		--max_step	400                                 \
+		--max_step	80                                 \
 		--max_fail	10                                  \
 		--num_threads 4; 
 
